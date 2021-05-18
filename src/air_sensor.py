31b3e708e5dc86d67d0a0984891ed1cbdde9f763
Michael Wagner <michael.wm.wagner@fau.de>
2021-05-18 17:07:46 +0200

    air sensor testing

--- src/air_sensor.py
+++ src/air_sensor.py
@@ -1,77 +1,126 @@
+#!/usr/bin/env python
 
-'''
-In our example of how to convert the BME680's gas resistance readings into a percentage indoor air quality (IAQ),
-we took a background reading for 5 minutes, then set this as the optimal gas resistance reading (100%), and also
-factored in humidity with a weighting of 75:25 for gas:humidity, as humidity also has an effect on IAQ (a relative humidity of 40% is optimal).
-
-Gas baseline: 683113.0155294954 Ohms, humidity baseline: 40.00 %RH
-
-'''
-import time
-import board
-from busio import I2C
-#import adafruit_bme680
 import bme680
+import time
 
+print("""read-all.py - Displays temperature, pressure, humidity, and gas.
 
+Press Ctrl+C to exit!
 
-#Gas baseline: 683113.0155294954 Ohms, humidity baseline: 40.00 %RH
-gas_baseline = 683113.0155294954
-# Set the humidity baseline to 40%, an optimal indoor humidity.
-hum_baseline = 40.0
-# This sets the balance between humidity and gas reading in the
-# calculation of air_quality_score (25:75, humidity:gas)
-hum_weighting = 0.25
-# Create library object using our Bus I2C port
-i2c = I2C(board.SCL, board.SDA)
+""")
 
-#bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c, debug=False)
 try:
     sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
 except (RuntimeError, IOError):
     sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
 
-# change this to match the location's pressure (hPa) at sea level
-sensor.sea_level_pressure = 1013.25
-while True:
-    print("\nTemperature: %0.1f C" % sensor.data.temperature)
-    #print("Gas: %d ohm" % bme680.gas)
-    print("Humidity: %0.1f %%" % sensor.data.humidity)
-    print("Pressure: %0.3f hPa" % sensor.data.pressure)
-    #print("Altitude = %0.2f meters" % bme680.altitude)
+# These calibration data can safely be commented
+# out, if desired.
 
-    try:
-        sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
-    except (RuntimeError, IOError):
-        sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
+print('Calibration data:')
+for name in dir(sensor.calibration_data):
 
-    if sensor.get_sensor_data() and sensor.data.heat_stable:
-        gas = sensor.data.gas_resistance
-        gas_offset = gas_baseline - gas
+    if not name.startswith('_'):
+        value = getattr(sensor.calibration_data, name)
 
-        hum = sensor.data.humidity
-        hum_offset = hum - hum_baseline
+        if isinstance(value, int):
+            print('{}: {}'.format(name, value))
 
-        # Calculate hum_score as the distance from the hum_baseline.
-        if hum_offset > 0:
-            hum_score = (100 - hum_baseline - hum_offset)
-            hum_score /= (100 - hum_baseline)
-            hum_score *= (hum_weighting * 100)
+# These oversampling settings can be tweaked to
+# change the balance between accuracy and noise in
+# the data.
 
-        else:
-            hum_score = (hum_baseline + hum_offset)
-            hum_score /= hum_baseline
-            hum_score *= (hum_weighting * 100)
+sensor.set_humidity_oversample(bme680.OS_2X)
+sensor.set_pressure_oversample(bme680.OS_4X)
+sensor.set_temperature_oversample(bme680.OS_8X)
+sensor.set_filter(bme680.FILTER_SIZE_3)
+sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
 
-        # Calculate gas_score as the distance from the gas_baseline.
-        if gas_offset > 0:
-            gas_score = (gas / gas_baseline)
-            gas_score *= (100 - (hum_weighting * 100))
+# ToDo: config method
 
-        else:
-            gas_score = 100 - (hum_weighting * 100)
+# Set the gas base_line to Michaels room
+gas_baseline = 683113.0155294954
 
-        # Calculate air_quality_score.
-        air_quality_score = hum_score + gas_score
+# Set the humidity baseline to 40%, an optimal indoor humidity.
+hum_baseline = 40.0
 
-    time.sleep(1)
+# This sets the balance between humidity and gas reading in the
+# calculation of air_quality_score (25:75, humidity:gas)
+hum_weighting = 0.25
+
+print('Gas baseline: {0} Ohms, humidity baseline: {1:.2f} %RH\n'.format(
+    gas_baseline,
+    hum_baseline))
+
+print('\n\nInitial reading:')
+for name in dir(sensor.data):
+    value = getattr(sensor.data, name)
+
+    if not name.startswith('_'):
+        print('{}: {}'.format(name, value))
+
+sensor.set_gas_heater_temperature(320)
+sensor.set_gas_heater_duration(150)
+sensor.select_gas_heater_profile(0)
+
+# Up to 10 heater profiles can be configured, each
+# with their own temperature and duration.
+# sensor.set_gas_heater_profile(200, 150, nb_profile=1)
+# sensor.select_gas_heater_profile(1)
+
+print('\n\nPolling:')
+try:
+    while True:
+        if sensor.get_sensor_data():
+            output = '{0:.2f} C,{1:.2f} hPa,{2:.2f} %RH'.format(
+                sensor.data.temperature,
+                sensor.data.pressure,
+                sensor.data.humidity,
+                air_quality_score)
+
+            if sensor.get_sensor_data() and sensor.data.heat_stable:
+                gas = sensor.data.gas_resistance
+                gas_offset = gas_baseline - gas
+
+                hum = sensor.data.humidity
+                hum_offset = hum - hum_baseline
+
+                # Calculate hum_score as the distance from the hum_baseline.
+                if hum_offset > 0:
+                    hum_score = (100 - hum_baseline - hum_offset)
+                    hum_score /= (100 - hum_baseline)
+                    hum_score *= (hum_weighting * 100)
+
+                else:
+                    hum_score = (hum_baseline + hum_offset)
+                    hum_score /= hum_baseline
+                    hum_score *= (hum_weighting * 100)
+
+                # Calculate gas_score as the distance from the gas_baseline.
+                if gas_offset > 0:
+                    gas_score = (gas / gas_baseline)
+                    gas_score *= (100 - (hum_weighting * 100))
+
+                else:
+                    gas_score = 100 - (hum_weighting * 100)
+
+                # Calculate air_quality_score.
+                air_quality_score = hum_score + gas_score
+
+                print('Gas: {0:.2f} Ohms,humidity: {1:.2f} %RH,air quality: {2:.2f}'.format(
+                    gas,
+                    hum,
+                    air_quality_score))
+
+            if sensor.data.heat_stable:
+                print('{0},{1} %'.format(
+                    output,
+                    air_quality_score))
+
+            else:
+                print(output)
+
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    pass
